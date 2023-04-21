52bd268a0a208ba388f7ae713221de5e88609308
Kyle Guadagno <kguadagno2@gmail.com>
2023-04-21 13:18:22 -0600

    4. Token Balances

--- test/Token.js
+++ test/Token.js
@@ -7,11 +7,16 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-    let token 
-
+    let token,
+        accounts,
+        deployer
+    
     beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token')
-        token = await Token.deploy('Kyle Guad Coin','KGC','1000000') 
+        token = await Token.deploy('Kyle Guad Coin', 'KGC', '1000000')
+
+        accounts = await ethers.getSigners()
+        deployer = accounts[0]
     })
 
     describe('Deployment', () => {
@@ -19,18 +24,23 @@ describe('Token', () => {
         const symbol = 'KGC'
         const decimals = '18'
         const totalSupply = '1000000'
+
         it('has correct name', async () => {
-        expect(await token.name()).to.equal(name)
-    })
+            expect(await token.name()).to.equal(name)
+        })
 
         it('has correct symbol', async () => {
-        expect(await token.symbol()).to.equal(symbol)
-    })
+            expect(await token.symbol()).to.equal(symbol)
+        })
         it('has correct decimals', async () => {
-        expect(await token.decimals()).to.equal(decimals)
-    })
+            expect(await token.decimals()).to.equal(decimals)
+        })
         it('has correct total supply', async () => {
-        expect(await token.totalSupply()).to.equal(tokens(totalSupply))
+            expect(await token.totalSupply()).to.equal(tokens(totalSupply))
+        })
+        it('assigns total supply to deployer', async () => {
+            console.log(deployer)
+            expect(await token.balanceOf(deployer.address)).to.equal(tokens(totalSupply))
+        })
     })
-})
 })
