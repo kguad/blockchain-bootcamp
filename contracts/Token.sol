19f0f13683219917f6fbe21f2bd67c4a5e69f4c9
Kyle Guadagno <kguadagno2@gmail.com>
2023-04-22 16:36:20 -0600

    7. Token Transfer Delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -41,17 +41,24 @@ contract Token {
     {
         //Require that sender has enough tokens to spend
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
 
-        //Deduct tokens from sender
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        //Credit tokens to receiver
-        balanceOf[_to] = balanceOf[_to] + _value;
-
-        //Emit Event
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
+        
 
         return true;
+    }
+
+    function _transfer(
+        address _from,
+        address _to,
+        uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+        balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(_from, _to, _value);
     }
 
 
@@ -65,4 +72,25 @@ contract Token {
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool success)
+    {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+
+        //Reset Allowance
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+
+        //spend tokens
+        _transfer(_from, _to, _value);
+        return true;
+    }  
+
 }
